5561f6ccabac92661ef605b7e88e6979a4a5d3af
bond50 <galavu10@gmail.com>
2019-09-06 15:07:46 +0300

    added address/urls.py

---
+++ address/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth import views as auth_views
+from accounts import forms
+
+app_name = 'address'
+
+urlpatterns = [
+
+
+]
--- booktime1/urls.py
+++ booktime1/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
                   path('accounts/', include("accounts.urls")),
                   path('contact/', include("contact.urls")),
                   path('main/', include("main.urls")),
+                  path('main/', include("address.urls")),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
