9db3e011ff02c00aeb21cfa8dc769c2cc25338b9
bond50 <galavu10@gmail.com>
2019-09-07 16:31:44 +0300

    added basket middleware

--- booktime1/settings.py
+++ booktime1/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'basket.middlewares.basket_middleware',
 ]
 
 ROOT_URLCONF = 'booktime1.urls'
