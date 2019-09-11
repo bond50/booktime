d2c9d488771bda55ab6eda916feba807b70a4198
bond50 <galavu10@gmail.com>
2019-09-11 06:31:44 +0300

    ALOT

--- booktime1/urls.py
+++ booktime1/urls.py
@@ -3,11 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
+from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('', TemplateView.as_view(template_name="index.html"), name='index'),
+                  # path('', TemplateView.as_view(template_name="index.html"), name='index'),
+                  path('', views.index, name='index'),
                   path('accounts/', include("accounts.urls")),
                   path('contact/', include("contact.urls")),
                   path('main/', include("main.urls")),
