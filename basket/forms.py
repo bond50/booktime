c33a1fb977d43c093c1dc9e781aa32c2e0dea039
bond50 <galavu10@gmail.com>
2019-09-16 12:43:56 +0300

    enhancing the basket functionality

--- basket/forms.py
+++ basket/forms.py
@@ -1,9 +1,6 @@
 from django.forms import inlineformset_factory
+from . import widgets
 from . import models
 
-BasketLineFormSet = inlineformset_factory(
-    models.Basket,
-    models.BasketLine,
-    fields=("quantity",),
-    extra=0,
-)
+BasketLineFormSet = inlineformset_factory(models.Basket, models.BasketLine, fields=("quantity",), extra=0,
+                                          widgets={"quantity": widgets.PlusMinusNumberInput()}, )
--- main/signals.py
+++ main/signals.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.signals import user_logged_in
+from basket.models import Basket
 from io import BytesIO
 import logging
 from PIL import Image
@@ -23,3 +25,21 @@ def generate_thumbnail(sender, instance, **kwargs):
     # set save=False, otherwise it will run in an infinite loop
     instance.thumbnail.save(instance.image.name, ContentFile(temp_thumb.read()), save=False, )
     temp_thumb.close()
+
+
+# @receiver(user_logged_in)
+# def merge_baskets_if_found(sender, user, request, **kwargs):
+#     anonymous_basket = getattr(request, "basket", None)
+#     if anonymous_basket:
+#         try:
+#             loggedin_basket = Basket.objects.get(user=user, status=Basket.OPEN)
+#             for line in anonymous_basket.basketline_set.all():
+#                 line.basket = loggedin_basket
+#                 line.save()
+#             anonymous_basket.delete()
+#             request.basket = loggedin_basket
+#             logger.info("Merged basket to id %d", loggedin_basket.id)
+#         except Basket.DoesNotExist:
+#             anonymous_basket.user = user
+#             anonymous_basket.save()
+#             logger.info("Assigned user to basket id %d", anonymous_basket.id, )
